4a1a18746755a285d6563d81625912cb90db7083
dormiha <dormi.fu@vipshop.com>
2016-12-09 10:12:11 +0800

    thirdcommit

--- src/dormi/Dormi.java
+++ src/dormi/Dormi.java
@@ -6,5 +6,6 @@ package dormi;
 public class Dormi {
     public static void main(String[] args){
         System.out.println("hi...");
+        System.out.println("woqu");
     }
 }
---
+++ src/dormi/Fcj.java
@@ -0,0 +1,8 @@
+package dormi;
+
+/**
+ * Created by dormi.fu on 2016/12/9.
+ */
+public class Fcj {
+    String name;
+}
